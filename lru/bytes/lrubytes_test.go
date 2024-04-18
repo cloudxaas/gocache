c9d4bd44cdadf21484dc5ffddf91cd8391290967
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2024-04-18 00:55:05 -0400

    Update lrubytes_test.go

--- lru/bytes/lrubytes_test.go
+++ lru/bytes/lrubytes_test.go
@@ -2,7 +2,58 @@ package lrubytes
 
 import (
     "testing"
+        "github.com/phuslu/lru"
 )
+
+// Helper function to convert byte slices to string for use as keys in the lru.Cache
+func bytesToString(b []byte) string {
+        return string(b)
+}
+
+func BenchmarkPhusluLRUPut(b *testing.B) {
+        cache := lru.NewLRUCache[string, []byte](32 * 1024)
+        keys := make([][]byte, 1000)
+        values := make([][]byte, 1000)
+        for i := 0; i < 1000; i++ {
+                keys[i] = []byte{byte(i)}
+                values[i] = make([]byte, 1024) // 1 KB values
+        }
+
+        b.ResetTimer()
+        for i := 0; i < b.N; i++ {
+                cache.Set(bytesToString(keys[i%1000]), values[i%1000])
+        }
+}
+
+func BenchmarkPhusluLRUGet(b *testing.B) {
+        cache := lru.NewLRUCache[string, []byte](32 * 1024)
+        for i := 0; i < 1000; i++ {
+                cache.Set(bytesToString([]byte{byte(i)}), make([]byte, 1024)) // 1 KB values
+        }
+        keys := make([][]byte, b.N)
+        for i := 0; i < b.N; i++ {
+                keys[i] = []byte{byte(i % 1000)}
+        }
+
+        b.ResetTimer()
+        for i := 0; i < b.N; i++ {
+                _, _ = cache.Get(bytesToString(keys[i]))
+        }
+}
+
+func BenchmarkPhusluLRUDelete(b *testing.B) {
+        cache := lru.NewLRUCache[string, []byte](32 * 1024)
+        keys := make([][]byte, 1000)
+        for i := 0; i < 1000; i++ {
+                keys[i] = []byte{byte(i)}
+                cache.Set(bytesToString(keys[i]), make([]byte, 1024)) // 1 KB values
+        }
+
+        b.ResetTimer()
+        for i := 0; i < b.N; i++ {
+                cache.Delete(bytesToString(keys[i%1000]))
+        }
+}
 
 
 
