f186bcf8d110cfbec2879c19aa7122f17b61dcd0
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2024-04-18 23:43:56 -0400

    Update lrubytes_test.go

--- lru/bytes/lrubytes_test.go
+++ lru/bytes/lrubytes_test.go
@@ -2,13 +2,11 @@ package lrubytes
 
 import (
     "testing"
-        "github.com/phuslu/lru"
+    "github.com/phuslu/lru"
+    cx "github.com/cloudxaas/gocx"
 )
 
 // Helper function to convert byte slices to string for use as keys in the lru.Cache
-func bytesToString(b []byte) string {
-        return string(b)
-}
 
 func BenchmarkPhusluLRUPut(b *testing.B) {
         cache := lru.NewLRUCache[string, []byte](32 * 1024)
@@ -21,14 +19,14 @@ func BenchmarkPhusluLRUPut(b *testing.B) {
 
         b.ResetTimer()
         for i := 0; i < b.N; i++ {
-                cache.Set(bytesToString(keys[i%1000]), values[i%1000])
+                cache.Set(cx.B2s(keys[i%1000]), values[i%1000])
         }
 }
 
 func BenchmarkPhusluLRUGet(b *testing.B) {
         cache := lru.NewLRUCache[string, []byte](32 * 1024)
         for i := 0; i < 1000; i++ {
-                cache.Set(bytesToString([]byte{byte(i)}), make([]byte, 1024)) // 1 KB values
+                cache.Set(cx.B2s([]byte{byte(i)}), make([]byte, 1024)) // 1 KB values
         }
         keys := make([][]byte, b.N)
         for i := 0; i < b.N; i++ {
@@ -37,7 +35,7 @@ func BenchmarkPhusluLRUGet(b *testing.B) {
 
         b.ResetTimer()
         for i := 0; i < b.N; i++ {
-                _, _ = cache.Get(bytesToString(keys[i]))
+                _, _ = cache.Get(cx.B2s(keys[i]))
         }
 }
 
@@ -46,12 +44,12 @@ func BenchmarkPhusluLRUDelete(b *testing.B) {
         keys := make([][]byte, 1000)
         for i := 0; i < 1000; i++ {
                 keys[i] = []byte{byte(i)}
-                cache.Set(bytesToString(keys[i]), make([]byte, 1024)) // 1 KB values
+                cache.Set(cx.B2s(keys[i]), make([]byte, 1024)) // 1 KB values
         }
 
         b.ResetTimer()
         for i := 0; i < b.N; i++ {
-                cache.Delete(bytesToString(keys[i%1000]))
+                cache.Delete(cx.B2s(keys[i%1000]))
         }
 }
 
