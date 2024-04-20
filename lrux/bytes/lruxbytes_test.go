8bf6d1915648df8e4ba820ef7b7bd7b5dc32eaba
Your Name <you@example.com>
2024-04-20 15:17:44 -0400

    update test

--- lrux/bytes/lruxbytes_test.go
+++ lrux/bytes/lruxbytes_test.go
@@ -5,15 +5,7 @@ import (
 
 	cx "github.com/cloudxaas/gocx"
 	"github.com/phuslu/lru"
-	"github.com/zeebo/xxh3"
 )
-
-// xxh3HashFunc wraps xxh3's Hash function to return uint32, adjusting to the expected hash function signature.
-func xxh3HashFunc(b []byte) uint32 {
-	return uint32(xxh3.Hash(b))
-}
-
-// Helper function to convert byte slices to string for use as keys in the lru.Cache
 
 func BenchmarkPhusluLRUSet(b *testing.B) {
 	cache := lru.NewLRUCache[string, []byte](1024 * 100)
@@ -61,7 +53,7 @@ func BenchmarkPhusluLRUDelete(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesSet(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, xxh3HashFunc)
+	cache := NewLRUCache(1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
 	keys := make([][]byte, 100000)
 	values := make([][]byte, 100000)
 	for i := 0; i < 100000; i++ {
@@ -75,7 +67,7 @@ func BenchmarkCXLRUBytesSet(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesGet(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, xxh3HashFunc)
+	cache := NewLRUCache(1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
 	for i := 0; i < 100000; i++ {
 		cache.Set([]byte{byte(i)}, make([]byte, 1024)) // 1 KB values
 	}
@@ -90,7 +82,7 @@ func BenchmarkCXLRUBytesGet(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesDel(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, xxh3HashFunc)
+	cache := NewLRUCache(1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
 	keys := make([][]byte, 100000)
 	for i := 0; i < 100000; i++ {
 		keys[i] = []byte{byte(i)}
@@ -104,7 +96,7 @@ func BenchmarkCXLRUBytesDel(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesSetParallel(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, xxh3HashFunc)
+	cache := NewLRUCache(1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
 	keys := make([][]byte, 100000)
 	values := make([][]byte, 100000)
 	for i := 0; i < 100000; i++ {
@@ -121,7 +113,7 @@ func BenchmarkCXLRUBytesSetParallel(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesGetParallel(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, xxh3HashFunc)
+	cache := NewLRUCache(1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
 	for i := 0; i < 100000; i++ {
 		cache.Set([]byte{byte(i)}, make([]byte, 1024)) // 1 KB values
 	}
@@ -140,7 +132,102 @@ func BenchmarkCXLRUBytesGetParallel(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesDelParallel(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, xxh3HashFunc)
+	cache := NewLRUCache(1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
+	keys := make([][]byte, 100000)
+	for i := 0; i < 100000; i++ {
+		keys[i] = []byte{byte(i)}
+		cache.Set(keys[i], make([]byte, 1024)) // 1 KB values
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for i := 0; pb.Next(); i++ {
+			cache.Del(keys[i%100000])
+		}
+	})
+}
+
+func BenchmarkCXLRUBytesShardedSet(b *testing.B) {
+	cache := NewShardedCache(16, 1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
+	keys := make([][]byte, 100000)
+	values := make([][]byte, 100000)
+	for i := 0; i < 100000; i++ {
+		keys[i] = []byte{byte(i)}
+		values[i] = make([]byte, 1024) // 1 KB values
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		cache.Set(keys[i%100000], values[i%100000])
+	}
+}
+
+func BenchmarkCXLRUBytesShardedGet(b *testing.B) {
+	cache := NewShardedCache(16, 1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
+	for i := 0; i < 100000; i++ {
+		cache.Set([]byte{byte(i)}, make([]byte, 1024)) // 1 KB values
+	}
+	keys := make([][]byte, b.N)
+	for i := 0; i < b.N; i++ {
+		keys[i] = []byte{byte(i % 100000)}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = cache.Get(keys[i])
+	}
+}
+
+func BenchmarkCXLRUBytesShardedDel(b *testing.B) {
+	cache := NewShardedCache(16, 1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
+	keys := make([][]byte, 100000)
+	for i := 0; i < 100000; i++ {
+		keys[i] = []byte{byte(i)}
+		cache.Set(keys[i], make([]byte, 1024)) // 1 KB values
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		cache.Del(keys[i%100000])
+	}
+}
+
+func BenchmarkCXLRUBytesShardedSetParallel(b *testing.B) {
+	cache := NewShardedCache(16, 1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
+	keys := make([][]byte, 100000)
+	values := make([][]byte, 100000)
+	for i := 0; i < 100000; i++ {
+		keys[i] = []byte{byte(i)}
+		values[i] = make([]byte, 1024) // 1 KB values
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for i := 0; pb.Next(); i++ {
+			cache.Set(keys[i%100000], values[i%100000])
+		}
+	})
+}
+
+func BenchmarkCXLRUBytesShardedGetParallel(b *testing.B) {
+	cache := NewShardedCache(16, 1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
+	for i := 0; i < 100000; i++ {
+		cache.Set([]byte{byte(i)}, make([]byte, 1024)) // 1 KB values
+	}
+
+	keys := make([][]byte, 100000)
+	for i := 0; i < 100000; i++ {
+		keys[i] = []byte{byte(i)}
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for i := 0; pb.Next(); i++ {
+			_, _ = cache.Get(keys[i%100000])
+		}
+	})
+}
+
+func BenchmarkCXLRUBytesShardedDelParallel(b *testing.B) {
+	cache := NewShardedCache(16, 1024*100, 1, func(key []byte) uint32 { return uint32(key[0]) })
 	keys := make([][]byte, 100000)
 	for i := 0; i < 100000; i++ {
 		keys[i] = []byte{byte(i)}
