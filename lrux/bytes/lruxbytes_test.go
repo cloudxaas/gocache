572d50640802dd556d4c96339ad9dc58fa01df13
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2024-04-21 04:17:19 -0400

    Update lruxbytes_test.go

--- lrux/bytes/lruxbytes_test.go
+++ lrux/bytes/lruxbytes_test.go
@@ -1,13 +1,118 @@
 package lruxbytes
 
 import (
+	//"time"
 	"testing"
 	"fmt"
 
 	cx "github.com/cloudxaas/gocx"
 	"github.com/phuslu/lru"
 	"github.com/maypok86/otter"
+	"github.com/elastic/go-freelru"
+	"github.com/cespare/xxhash/v2"
+	hashicorp "github.com/hashicorp/golang-lru/v2"
 )
+
+func hashStringXXHASH(s string) uint32 {
+	return uint32(xxhash.Sum64String(s))
+}
+
+
+func BenchmarkHashicorpLRUSet(b *testing.B) {
+    cache, _ := hashicorp.New[string, []byte](1024*100)
+    keys := make([]string, 100000)
+    values := make([][]byte, 100000)
+    for i := 0; i < 100000; i++ {
+        keys[i] = string([]byte{byte(i)})
+        values[i] = make([]byte, 1024) // 1 KB values
+    }
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        cache.Add(keys[i%100000], values[i%100000])
+    }
+}
+
+func BenchmarkHashicorpLRUGet(b *testing.B) {
+    cache, _ := hashicorp.New[string, []byte](1024*100)
+    keys := make([]string, 100000)
+    for i := 0; i < 100000; i++ {
+        keys[i] = string([]byte{byte(i)})
+        cache.Add(keys[i], make([]byte, 1024)) // 1 KB values
+    }
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        _, _ = cache.Get(keys[i%100000])
+    }
+}
+
+func BenchmarkHashicorpLRURemove(b *testing.B) {
+    cache, _ := hashicorp.New[string, []byte](1024*100)
+    keys := make([]string, 100000)
+    for i := 0; i < 100000; i++ {
+        keys[i] = string([]byte{byte(i)})
+        cache.Add(keys[i], make([]byte, 1024)) // 1 KB values
+    }
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        _ = cache.Remove(keys[i%100000])
+    }
+}
+
+func BenchmarkGoFreeLRUSet(b *testing.B) {
+	lru, err := freelru.New[string, uint64](1024*100, hashStringXXHASH)
+	if err != nil {
+		b.Fatal(err)
+	}
+	keys := make([]string, 100000)
+	values := make([]uint64, 100000)
+	for i := 0; i < 100000; i++ {
+		keys[i] = fmt.Sprintf("key%d", i)
+		values[i] = uint64(i)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		lru.Add(keys[i%100000], values[i%100000])
+	}
+}
+
+func BenchmarkGoFreeLRUGet(b *testing.B) {
+	lru, err := freelru.New[string, uint64](1024*100, hashStringXXHASH)
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < 100000; i++ {
+		lru.Add(fmt.Sprintf("key%d", i), uint64(i))
+	}
+	keys := make([]string, b.N)
+	for i := 0; i < b.N; i++ {
+		keys[i] = fmt.Sprintf("key%d", i%100000)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = lru.Get(keys[i])
+	}
+}
+
+func BenchmarkGoFreeLRURemove(b *testing.B) {
+        lru, err := freelru.New[string, uint64](1024*100, hashStringXXHASH)
+        if err != nil {
+                b.Fatal(err)
+        }
+        for i := 0; i < 100000; i++ {
+                lru.Add(fmt.Sprintf("key%d", i), uint64(i))
+        }
+        keys := make([]string, b.N)
+        for i := 0; i < b.N; i++ {
+                keys[i] = fmt.Sprintf("key%d", i%100000)
+        }
+
+        b.ResetTimer()
+        for i := 0; i < b.N; i++ {
+                _ = lru.Remove(keys[i])
+        }
+}
 
 func BenchmarkOtterSet(b *testing.B) {
 	cache, err := otter.MustBuilder[string, string](1024 * 100).
@@ -141,7 +246,7 @@ func BenchmarkPhusluLRUDelete(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesSet(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, FNV1aHash)
+	cache := NewLRUCache(1024*100, 1, FNV1aHash) //you are supposed to put 2 * 1000bytes instead for fairer comparison. so means... 1000*1000*2*1000, if you have better ways to gauge how much fairer, please do suggest. this cache setting evicts a lot of items heavily and thus results in lower hit ratio, put it higher as it is limited by memory capacity and not item capacity 
 	keys := make([][]byte, 100000)
 	values := make([][]byte, 100000)
 	for i := 0; i < 100000; i++ {
@@ -155,7 +260,7 @@ func BenchmarkCXLRUBytesSet(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesGet(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, FNV1aHash)
+	cache := NewLRUCache(1024*100, 1, FNV1aHash) //you are supposed to put 2 * 1000bytes instead for fairer comparison. so means... 1000*1000*2*1000, if you have better ways to gauge how much fairer, please do suggest. this cache setting evicts a lot of items heavily and thus results in lower hit ratio, put it higher as it is limited by memory capacity and not item capacity 
 	for i := 0; i < 100000; i++ {
 		cache.Set([]byte{byte(i)}, make([]byte, 1024)) // 1 KB values
 	}
@@ -170,7 +275,7 @@ func BenchmarkCXLRUBytesGet(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesDel(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, FNV1aHash)
+	cache := NewLRUCache(1024*100, 1, FNV1aHash) //you are supposed to put 2 * 1000bytes instead for fairer comparison. so means... 1000*1000*2*1000, if you have better ways to gauge how much fairer, please do suggest. this cache setting evicts a lot of items heavily and thus results in lower hit ratio, put it higher as it is limited by memory capacity and not item capacity 
 	keys := make([][]byte, 100000)
 	for i := 0; i < 100000; i++ {
 		keys[i] = []byte{byte(i)}
@@ -184,7 +289,7 @@ func BenchmarkCXLRUBytesDel(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesSetParallel(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, FNV1aHash)
+	cache := NewLRUCache(1024*100, 1, FNV1aHash) //you are supposed to put 2 * 1000bytes instead for fairer comparison. so means... 1000*1000*2*1000, if you have better ways to gauge how much fairer, please do suggest. this cache setting evicts a lot of items heavily and thus results in lower hit ratio, put it higher as it is limited by memory capacity and not item capacity 
 	keys := make([][]byte, 100000)
 	values := make([][]byte, 100000)
 	for i := 0; i < 100000; i++ {
@@ -201,7 +306,7 @@ func BenchmarkCXLRUBytesSetParallel(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesGetParallel(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, FNV1aHash)
+	cache := NewLRUCache(1024*100, 1, FNV1aHash) //you are supposed to put 2 * 1000bytes instead for fairer comparison. so means... 1000*1000*2*1000, if you have better ways to gauge how much fairer, please do suggest. this cache setting evicts a lot of items heavily and thus results in lower hit ratio, put it higher as it is limited by memory capacity and not item capacity 
 	for i := 0; i < 100000; i++ {
 		cache.Set([]byte{byte(i)}, make([]byte, 1024)) // 1 KB values
 	}
@@ -220,7 +325,7 @@ func BenchmarkCXLRUBytesGetParallel(b *testing.B) {
 }
 
 func BenchmarkCXLRUBytesDelParallel(b *testing.B) {
-	cache := NewLRUCache(1024*100, 1, FNV1aHash)
+	cache := NewLRUCache(1024*100, 1, FNV1aHash) //you are supposed to put 2 * 1000bytes instead for fairer comparison. so means... 1000*1000*2*1000, if you have better ways to gauge how much fairer, please do suggest. this cache setting evicts a lot of items heavily and thus results in lower hit ratio, put it higher as it is limited by memory capacity and not item capacity 
 	keys := make([][]byte, 100000)
 	for i := 0; i < 100000; i++ {
 		keys[i] = []byte{byte(i)}
